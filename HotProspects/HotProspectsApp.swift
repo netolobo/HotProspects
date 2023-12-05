be3f97c430b850a9e73b9c3a953e0768abc53470
netolobo <desilio@gmail.com>
2023-12-05 13:38:38 -0800

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Neto Lobo on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
